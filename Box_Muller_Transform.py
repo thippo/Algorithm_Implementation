53d3ef86716b410bd564e7d66c90901d7bdc4195
thippo <thippo@163.com>
2015-07-27 21:51:13 +0800

    branch

---
+++ Box_Muller_Transform.py
@@ -0,0 +1,18 @@
+#writen by thippo
+#python version 3.4.3
+
+class box_muller_transform():
+    import random
+    import math
+    def __init__(self,mean=0,sd=1):
+        self.mean=mean
+        self.sd=sd
+    def __call__(self):
+        u1=self.random.random()
+        u2=self.random.random()
+        Z=self.math.sqrt(-2*self.math.log(u2))*self.math.cos(2*self.math.pi*u1)
+        return self.mean+(Z*self.sd)
+
+if __name__=='__main__':
+    sampling=box_muller_transform(2,3)
+    print(sampling())
--- Rejection_Sampling.py
+++ Rejection_Sampling.py
@@ -15,19 +15,19 @@ class rejection_sampling():
     def __call__(self):
         switch=0
         while(switch==0):
-            u=random.random()
-            x=random.uniform(self.sampling_interval[0],self.sampling_interval[1])
+            u=self.random.random()
+            x=self.random.uniform(self.sampling_interval[0],self.sampling_interval[1])
             if u<self.target_function(x)/self.sampling_function(x):
                 switch=1
                 return x
-    def get_sampling_array(self,graphic=True):
-        u=np.array(np.random.rand(self.sampling_times))
-        x=np.array([random.uniform(self.sampling_interval[0],self.sampling_interval[1]) for x in range(self.sampling_times)])
+    def sampling_array(self,graphic=True):
+        u=self.np.array(self.np.random.rand(self.sampling_times))
+        x=self.np.array([self.random.uniform(self.sampling_interval[0],self.sampling_interval[1]) for x in range(self.sampling_times)])
         x_ratio=self.target_function(x)/self.sampling_function(x)
         if graphic:
-            plt.hist(x[u<x_ratio],np.arange(self.sampling_interval[0], self.sampling_interval[1], 0.1),normed=True)
-            plt.plot(range(self.sampling_interval[0],self.sampling_interval[1]),[self.target_function(x) for x in range(self.sampling_interval[0],self.sampling_interval[1])])
-            plt.show()
+            self.plt.hist(x[u<x_ratio],np.arange(self.sampling_interval[0], self.sampling_interval[1], 0.1),normed=True)
+            self.plt.plot(range(self.sampling_interval[0],self.sampling_interval[1]),[self.target_function(x) for x in range(self.sampling_interval[0],self.sampling_interval[1])])
+            self.plt.show()
         return x[u<x_ratio]
 
 if __name__=='__main__':
@@ -37,4 +37,5 @@ if __name__=='__main__':
     def sampling_function(x):
         return 0.8
     a=rejection_sampling(target_function,sampling_function,(-6,20),10000)
-    a.get_sampling_array(graphic=True)
+    a.sampling_array(graphic=True)
+    print(a())
