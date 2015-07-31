c50dee90b08bbf9bd023ea9876979df9f558facb
thippo <thippo@163.com>
2015-07-31 08:48:42 +0800

    class in class

--- Bloom_Filter.py
+++ Bloom_Filter.py
@@ -1,36 +1,37 @@
 #writen by thippo
 #python version 3.4.3
-#reference http://blog.csdn.net/demon24/article/details/8537665
-
-import BitVector
-
-class SimpleHash():  
-    
-    def __init__(self, cap, seed):
-        self.cap = cap
-        self.seed = seed
-    
-    def hash(self, value):
-        ret = 0
-        for i in range(len(value)):
-            ret += self.seed*ret + ord(value[i])
-        return (self.cap-1) & ret    
+#reference http://blog.csdn.net/demon24/article/details/8537665    
 
 class BloomFilter():
     
+    import BitVector
+    
+    class SimpleHash():  
+
+        def __init__(self, cap, seed):
+            self.cap = cap
+            self.seed = seed
+
+        def hash(self, value):
+            ret = 0
+            for i in range(len(value)):
+                ret += self.seed*ret + ord(value[i])
+            return (self.cap-1) & ret
+
     def __init__(self,bit_size=25,hash_count=7):
         self.bit_size = 1 << bit_size
         self.seeds = list(range(hash_count))
-        self.bitset = BitVector.BitVector(size=self.bit_size)
+        self.bitset = self.BitVector.BitVector(size=self.bit_size)
         self.hashfunction = []
         
         for i in range(len(self.seeds)):
-            self.hashfunction.append(SimpleHash(self.bit_size, self.seeds[i]))
-        
+            self.hashfunction.append(self.SimpleHash(self.bit_size, self.seeds[i]))
+   
     def insert(self, value):
         for f in self.hashfunction:
             loc = f.hash(value)
             self.bitset[loc] = 1
+			
     def lookup(self, value):
         if value == None:
             return False
@@ -42,7 +43,7 @@ class BloomFilter():
 
 def main():
     a=iter(['a','b','c','a'])
-    bloomfilter = BloomFilter()
+    bloomfilter = BloomFilter(bit_size=10,hash_count=5)
     while True:
         try:
             url = a.__next__()
